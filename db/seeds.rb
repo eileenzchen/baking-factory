52ec816bcabf0dfa64e4bdd6191d2bca1708f68f
Eileen Chen <ezchen@andrew.cmu.edu>
2019-05-21 16:10:12 -0400

    retry create and migrate db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# User.destroy_all # clobber everything in DB first
+# Customer.destroy_all
+# mark_user = User.create(username: "mark", password: "secret", password_confirmation: "secret", role: "admin")
+
+# mark = Customer.create(first_name: "Mark", last_name: "Heimann", email: "mark@thebakingfactory.com", phone: "412-268-2323", user_id: mark_user.id, active: true)
+
+
