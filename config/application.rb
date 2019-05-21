bd9953cd5eb22df7efee5ca63aecfd2bf6825a4d
Eileen Chen <ezchen@andrew.cmu.edu>
2019-05-21 00:09:52 -0400

    set assets precompile to false in application.rb

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ module BakingFactory
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Eastern Time (US & Canada)'
-
+    config.assets.initialize_on_precompile = false
+    
   end
 end
